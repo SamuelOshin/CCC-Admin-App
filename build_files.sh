ced3d07a206a2bc64a7fecba1274dca7b7d71297
SamuelOshin <samuelt.oshin@gmail.com>
2024-03-06 20:04:20 +0100

    Update build_files.sh script

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
- echo "BUILD START"
- python3.11.8 -m pip install -r requirements.txt
- python3.11.8 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+#!/bin/bash
+
+echo "BUILD START"
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
+echo "BUILD END"
