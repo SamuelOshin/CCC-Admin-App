9bad68d2ce2b5241c9b2fb9a6bed5b1fcaa57290
SamuelOshin <samuelt.oshin@gmail.com>
2024-03-06 18:23:13 +0100

    Update settings.py and wsgi.py files, commented db and added two files

--- cccadminapp/settings.py
+++ cccadminapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&znf+lys)k=mham#-3_v067)_0i^#p3_4))v(2(o^wnc^7p4jb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -85,16 +85,16 @@ WSGI_APPLICATION = 'cccadminapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'clergydb',
-        'USER': 'root',
-        'PASSWORD': 'Terigbade@1',
-        'HOST': 'localhost',  # or 'localhost'
-        'PORT': '3306',       # MySQL default port
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'clergydb',
+#         'USER': 'root',
+#         'PASSWORD': 'Terigbade@1',
+#         'HOST': 'localhost',  # or 'localhost'
+#         'PORT': '3306',       # MySQL default port
+#     }
+# }
 
 
 
@@ -133,12 +133,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'clergy_registration/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'clergy_registration/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL =''
-MEDIA_ROOT = ''
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
