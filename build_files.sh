a53e45d1b303d6ba5170a5cdb0d203e60667a424
SamuelOshin <samuelt.oshin@gmail.com>
2024-03-08 22:59:47 +0100

    Add virtual environment activation and collectstatic command to build script

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "BUILD START"
+source /opt/render/project/src/Scripts/activate
 python3 manage.py collectstatic --noinput --clear
 python3 -m pip install -r requirements.txt
 echo "BUILD END"
