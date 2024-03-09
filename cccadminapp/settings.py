fbc55007216bf8028903ba3548882749fc09b3d1
SamuelOshin <samuelt.oshin@gmail.com>
2024-03-09 02:45:58 +0100

    changed whitenoise settings

--- cccadminapp/settings.py
+++ cccadminapp/settings.py
@@ -147,11 +147,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STORAGES = {
     # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": 'whitenoise.storage.CompressedStaticFilesStorage',
     },
 }
 STATICFILES_DIRS = [
