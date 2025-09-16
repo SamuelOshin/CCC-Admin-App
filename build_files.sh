2a5f58767a1292fc2f003e6783eb478e12c3e4ae
SamuelOshin <samuelt.oshin@gmail.com>
2025-09-16 19:32:39 +0100

    chore(build): add database migration step to build script

--- build_files.sh
+++ build_files.sh
@@ -7,6 +7,9 @@ source /opt/render/project/venv/bin/activate
 # Install dependencies first
 python3 -m pip install -r requirements.txt
 
+# Run database migrations
+python3 manage.py migrate
+
 # Now run Django commands
 python3 manage.py collectstatic --noinput --clear
 
